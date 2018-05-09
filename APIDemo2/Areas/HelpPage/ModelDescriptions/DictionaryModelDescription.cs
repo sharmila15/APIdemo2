a572937b3b1058a92dd8170b693bba77f40f1527
sharmila15Angular <sthumurgoti15@gmail.com>
2018-05-09 11:39:40 -0400

    fifth commit

---
+++ APIDemo2/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace APIDemo2.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ APIDemo2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="APIDemo2.WebApiApplication" Language="C#" %>
